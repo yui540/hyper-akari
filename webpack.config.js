40352428d756657ed2cdc8ad201d5a7ba510bf50
yui540 <tabletennis540@gmail.com>
2018-07-06 16:15:33 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const mode = process.env.MODE || 'production'
+
+module.exports = {
+  mode: mode,
+  entry: "./src/index.js",
+  output: {
+    path: `${ __dirname }`,
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
