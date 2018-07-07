7f3345843b94f0d25955b026758f32bab75e2b81
yui540 <tabletennis540@gmail.com>
2018-07-07 10:19:59 +0900

    add: 画面全体の背景色をcssで指定する

--- src/stylesheets/index.js
+++ src/stylesheets/index.js
@@ -1,3 +1,4 @@
+import root from './root'
 import header from './header'
 
 export const termCSS = `
@@ -5,5 +6,6 @@ export const termCSS = `
 `
 
 export const css = `
+  ${ root }
   ${ header }
 `
---
+++ src/stylesheets/root.js
@@ -0,0 +1,7 @@
+import { backgroundColor } from '../colors'
+
+export default `
+  .hyper_main {
+    background-color: ${ backgroundColor };
+  }
+`
